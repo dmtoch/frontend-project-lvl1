6d8e6c080046840ebfefdd477f0bf85031177cdc
Daria Tochenaya <d.tochenaya@gmail.com>
2021-02-17 17:04:19 +0300

    fix: fix bug with generating numbers array

--- src/games/even.js
+++ src/games/even.js
@@ -3,12 +3,13 @@ import getRandomNumber from '../helpers.js';
 const isEven = (num) => num % 2 === 0;
 
 const even = () => {
-  const expression = [];
+  const expressions = [];
   for (let i = 0; i < 3; i += 1) {
-    expression.push(getRandomNumber());
+    const number = getRandomNumber();
+    expressions.push(number);
   }
 
-  const questionsWithAnswers = expression.map((number) => ({
+  const questionsWithAnswers = expressions.map((number) => ({
     question: number,
     correctAnswer: isEven(number) ? 'yes' : 'no',
   }));
